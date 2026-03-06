af9409c68b6c715a5f5387a50893129a869c7f98
Sammeta Nakshathra <sammetanakshatra701@gmail.com>
2026-03-06 22:52:16 +0530

    Add files via upload

---
+++ bpf.m
@@ -0,0 +1,66 @@
+clc;
+close all;
+clear all;
+n=input('enter the order of the filter');
+wc=input('enter the cutoff frequency');
+fs=input('enter the sampling frequency');
+w1=2*wc/fs;
+n1=n+1;
+%design of fir bandpass filter using rectangular window 
+y1 = rectwin(n1); %rectangular window function
+b1=fir1(n,w1,'bandpass',y1); % fir bandpass filter design using window
+[h1 om]=freqz(b1,1,256);
+m1=20*log10(abs(h1));
+subplot(3,2,1) 
+plot(om/pi,m1); grid
+ylabel('Gain in dB'); xlabel('Normalized frequency');
+title('FIR bandpass using rectangular window'); 
+axis([0 1 -100 10])
+%design of fir bandpass filter using trangular window 
+y2=triang(n1); %traingular window function
+b2=fir1(n,w1,'bandpass',y2);
+[h2 om]=freqz(b2,1,256);
+m2=20*log10(abs(h2));
+subplot(3,2,2) ;plot(om/pi,m2);
+ylabel('Gain in dB'); xlabel('Normalized frequency');
+title('FIR bandpass using trangular window'); 
+axis([0 1 -100 10]) 
+%design of fir bandpass filter using hamming window 
+y3=hamming(n1); %hamming window function
+b3=fir1(n,w1,'bandpass',y3);
+[h3 om]=freqz(b3,1,256);
+m3=20*log10(abs(h3));
+subplot(3,2,3); plot(om/pi,m3);  grid
+ylabel('Gain in dB'); xlabel('Normalized frequency');
+title('FIR bandpass using hamming window'); 
+axis([0 1 -100 10])
+%design of fir bandpass filter using hanning window 
+y4 = hann(n1); %hanning window function
+b4=fir1(n,w1,'bandpass',y4);
+[h4 om]=freqz(b4,1,256);
+m4=20*log10(abs(h4));
+subplot(3,2,4) 
+plot(om/pi,m4);  grid
+ylabel('Gain in dB'); xlabel('Normalized frequency');
+title('FIR bandpass using hanning window'); 
+axis([0 1 -100 10])
+%design of fir bandpass filter using blackman window 
+y5 = blackman(n1); %blackman window function
+b5=fir1(n,w1,'bandpass',y5);
+[h5 om]=freqz(b5,1,256);
+m5=20*log10(abs(h5));
+subplot(3,2,5) 
+plot(om/pi,m5);  grid
+ylabel('Gain in dB'); xlabel('Normalized frequency');
+title('FIR bandpass using blackman window'); 
+axis([0 1 -100 10])
+%design of fir bandpass filter using kaiser window 
+y6 = kaiser(n1,5); %kaiser window function
+b6=fir1(n,w1,'bandpass',y6);
+[h6 om]=freqz(b6,1,256);
+m6=20*log10(abs(h6));
+subplot(3,2,6) 
+plot(om/pi,m6); grid
+ylabel('Gain in dB'); xlabel('Normalized frequency');
+title('FIR bandpass using kaiser window'); 
+axis([0 1 -100 10])
